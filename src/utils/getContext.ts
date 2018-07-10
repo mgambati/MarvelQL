57740cf4bdd4ba6e7e793f5233c51d6bf151e856
Rohit <rohit@novvuminc.com>
2018-07-10 12:06:00 -0700

    marvel character model sample

--- src/utils/getContext.ts
+++ src/utils/getContext.ts
@@ -1,27 +1,29 @@
 import * as jwt from "jsonwebtoken";
 import { Prisma } from "../generated/prisma";
 import MarvelApiModel from "../models/MarvelApiModel";
+import CharacterModel from "../models/CharacterModel";
 export interface Context {
-	db: Prisma;
-	api: MarvelApiModel;
-	request: any;
+  db: Prisma;
+  api: MarvelApiModel;
+  charactersModel: CharacterModel;
+  request: any;
 }
 
 export function getUserId(ctx: Context) {
-	const Authorization = ctx.request.get("Authorization");
-	if (Authorization) {
-		const token = Authorization.replace("Bearer ", "");
-		const { userId } = jwt.verify(token, process.env.APP_SECRET) as {
-			userId: string;
-		};
-		return userId;
-	}
+  const Authorization = ctx.request.get("Authorization");
+  if (Authorization) {
+    const token = Authorization.replace("Bearer ", "");
+    const { userId } = jwt.verify(token, process.env.APP_SECRET) as {
+      userId: string;
+    };
+    return userId;
+  }
 
-	throw new AuthError();
+  throw new AuthError();
 }
 
 export class AuthError extends Error {
-	constructor() {
-		super("Not authorized");
-	}
+  constructor() {
+    super("Not authorized");
+  }
 }
